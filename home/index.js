b9862323f6dc6700c8d5f8ccf85475558f426be0
Leozin89python <leozin069@gmail.com>
2021-09-13 22:41:46 -0300

    demo

--- home/index.js
+++ home/index.js
@@ -69,14 +69,13 @@ $('#site').click(function(){
 
 
 function load_table(){
-    const id_area       = doc.getElementById('id')
     const name_area     = doc.getElementById('name')
     const job_area      = doc.getElementById('job')
     const sector_area   = doc.getElementById('sector')
     const salary_area   = doc.getElementById('salary')
 
 
-   const url = 'http://localhost:3003'
+   const url = 'https://app-workers.herokuapp.com/'
 
    const config = {
        method:'GET'
@@ -86,22 +85,19 @@ function load_table(){
                 .then(people => people.json())
                 .then(people => {
                     people.map(e=>{
-                        let id   = e.id
-                        create_data(id_area,id,'ul')
-
-                        let name = e.name.substring(0,15)
+                        let name = e.workername.substring(0,15)
                         create_data(name_area,name,'ul')
 
-                        let job = e.job.substring(0,15)
+                        let job = e.workerjob.substring(0,15)
                         create_data(job_area,job,'ul')
 
-                        let sector = e.sector.substring(0,15)
+                        let sector = e.workersector.substring(0,15)
                         create_data(sector_area,sector,'ul')
 
-                        let salary = e.salary + '$'
+                        let salary = e.workersalary + '$'
                         create_data(salary_area,salary,'ul')
                     })
-                })
+                })  
                 .catch()
 }
 
@@ -128,13 +124,12 @@ function search_table(){
                     .then(search => search.json())
                     .then(search =>{
                         search.filter(e=>{
-                                let id = e.id 
                                 let name = e.name.substring(0,15) 
                                 let job = e.job.substring(0,15) 
                                 let sector = e.sector.substring(0,15) 
                                 let salary = e.salary + '$' 
 
-                                searchPeople(id,name,job,sector,salary)
+                                searchPeople(name,job,sector,salary)
                         })
                     })
                     .catch(err => console.log(err))
@@ -167,8 +162,7 @@ setInterval(function(){
 },90000);
 
 
-function searchPeople(id, name, job, sector, salary){
-    const id_area        = doc.getElementById('idsc')
+function searchPeople(name, job, sector, salary){
     const name_area      = doc.getElementById('namesc')
     const job_area       = doc.getElementById('jobsc')
     const sector_area    = doc.getElementById('sectorsc')
@@ -176,7 +170,6 @@ function searchPeople(id, name, job, sector, salary){
 
     search.onkeyup = function(){
         if(search.value != ''){
-           id_area.innerHTML     = '<ul>' + id     + '</ul>'
            name_area.innerHTML   = '<ul>' + name   + '</ul>'
            job_area.innerHTML    = '<ul>' + job    + '</ul>'
            sector_area.innerHTML = '<ul>' + sector + '</ul>'
